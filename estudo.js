441bce6e04240919e32a12e629854f95dc523ded
biskelahub <contato.hotto@gmail.com>
2025-06-26 11:15:19 -0300

    meu primeiro commit

---
+++ estudo.js
@@ -0,0 +1,21 @@
+let fruta = 'banana';
+
+switch (fruta) {
+    case 'laranja': 
+    console.log('A fruta é uma laranja');
+    break;
+
+    case 'banana':
+        console.log('A fruta é uma banana');
+    break;  
+
+    case 'maçã':  
+        console.log('A fruta é uma maçã');                  
+    break;
+}
+
+
+for (let = contador= 0; contador < 10; contador++) {
+    console.log('Contador:', contador);
+}
+
